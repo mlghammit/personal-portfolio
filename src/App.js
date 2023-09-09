05e10b4d0934ab034875909d8fe3ff5b11d2b462
mlghammit <58157953+mlghammit@users.noreply.github.com>
2023-09-08 22:26:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import Hamza from './components/Hamza'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -10,15 +10,17 @@ import './App.scss'
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Hamza />} />
-          <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Project />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Hamza />} />
+            <Route path="about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </>
   )
 }
